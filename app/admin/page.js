800e312c5ea47de5ff1e0353425ca4b4ef96b727
Gideon Nieuwenhuis <greatresourcesgroupbv@gmail.com>
2024-11-30 21:26:52 +0100

    Update CMS configuration

--- app/admin/page.js
+++ app/admin/page.js
@@ -1,25 +1,19 @@
 'use client'
 
 import { useEffect } from 'react'
-import { useRouter } from 'next/navigation'
 
 export default function Admin() {
-  const router = useRouter()
-
   useEffect(() => {
-    // Check if window is defined (browser environment)
-    if (typeof window !== 'undefined') {
-      // When the component mounts, dynamically import the Netlify CMS script
-      (async () => {
-        const CMS = (await import('netlify-cms-app')).default
+    (async () => {
+      if (typeof window !== 'undefined') {
+        const CMS = (await import('decap-cms-app')).default
         CMS.init()
-      })()
-    }
+      }
+    })()
   }, [])
 
   return (
     <div>
-      {/* The CMS will automatically mount to this div */}
       <div id="nc-root" />
     </div>
   )
--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,12 @@
- 
+import Script from 'next/script'
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <Script src="https://identity.netlify.com/v1/netlify-identity-widget.js" strategy="beforeInteractive" />
+      </head>
+      <body>{children}</body>
+    </html>
+  )
+} 
