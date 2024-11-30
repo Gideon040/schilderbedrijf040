4cbb1fb3a7e836667787bdb5d36711d5fa3d588c
Gideon Nieuwenhuis <greatresourcesgroupbv@gmail.com>
2024-11-30 21:43:52 +0100

    Add project context and update build scripts

--- src/App.tsx
+++ src/App.tsx
@@ -19,32 +19,38 @@ import { TermsPage } from './pages/TermsPage';
 import { OverOnsPage } from './pages/OverOnsPage';
 import { ProjectDetailsProvider } from './contexts/ProjectDetailsContext';
 import { BlogPostProvider } from './contexts/BlogPostContext';
+import { BlogProvider } from './contexts/BlogContext'
+import { ProjectProvider } from './contexts/ProjectContext'
 
 function App() {
   return (
-    <ProjectDetailsProvider>
+    <BlogProvider>
       <BlogPostProvider>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/diensten/binnenschilderwerk" element={<BinnenschilderwerkPage />} />
-          <Route path="/diensten/houtrot-sanering" element={<HoutrotSaneringPage />} />
-          <Route path="/diensten/behangen" element={<BehangenPage />} />
-          <Route path="/diensten/lakwerk" element={<LakwerkPage />} />
-          <Route path="/diensten/latex-spuiten" element={<LatexSpuitenPage />} />
-          <Route path="/projecten" element={<ProjectsPage />} />
-          <Route path="/projecten/:id" element={<ProjectDetailPage />} />
-          <Route path="/waarom-ons" element={<WaaromOnsPage />} />
-          <Route path="/over-ons" element={<OverOnsPage />} />
-          <Route path="/contact" element={<ContactPage />} />
-          <Route path="/faq" element={<FAQPage />} />
-          <Route path="/blog" element={<BlogPage />} />
-          <Route path="/blog/:id" element={<BlogPostPage />} />
-          <Route path="/blog/verf/:slug" element={<BlogPostDetailPage />} />
-          <Route path="/privacy" element={<PrivacyPolicyPage />} />
-          <Route path="/algemene-voorwaarden" element={<TermsPage />} />
-        </Routes>
+        <ProjectProvider>
+          <ProjectDetailsProvider>
+            <Routes>
+              <Route path="/" element={<HomePage />} />
+              <Route path="/diensten/binnenschilderwerk" element={<BinnenschilderwerkPage />} />
+              <Route path="/diensten/houtrot-sanering" element={<HoutrotSaneringPage />} />
+              <Route path="/diensten/behangen" element={<BehangenPage />} />
+              <Route path="/diensten/lakwerk" element={<LakwerkPage />} />
+              <Route path="/diensten/latex-spuiten" element={<LatexSpuitenPage />} />
+              <Route path="/projecten" element={<ProjectsPage />} />
+              <Route path="/projecten/:id" element={<ProjectDetailPage />} />
+              <Route path="/waarom-ons" element={<WaaromOnsPage />} />
+              <Route path="/over-ons" element={<OverOnsPage />} />
+              <Route path="/contact" element={<ContactPage />} />
+              <Route path="/faq" element={<FAQPage />} />
+              <Route path="/blog" element={<BlogPage />} />
+              <Route path="/blog/:id" element={<BlogPostPage />} />
+              <Route path="/blog/verf/:slug" element={<BlogPostDetailPage />} />
+              <Route path="/privacy" element={<PrivacyPolicyPage />} />
+              <Route path="/algemene-voorwaarden" element={<TermsPage />} />
+            </Routes>
+          </ProjectDetailsProvider>
+        </ProjectProvider>
       </BlogPostProvider>
-    </ProjectDetailsProvider>
+    </BlogProvider>
   );
 }
 
